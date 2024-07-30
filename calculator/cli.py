7bf15d515bc9345b8860b09fb7d57cd7f53a22d3
Jesus Hincapie <jahlon@outlook.com>
2024-07-30 13:50:43 -0500

    add calc functions

---
+++ calculator/cli.py
@@ -0,0 +1,17 @@
+import click
+
+
+from calculator.model import Calculator
+
+
+
+@click.group()
+@click.pass_context
+def calc(ctx: click.Context):
+    """ A simple calculator"""
+
+    ctx.obj = {"calculator_object", Calculator()}
+
+
+
+
